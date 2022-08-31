0aa4c598d7c262f64df53302f1f5dfc7e027ab02
Zawadi <zawadi.opoti@student.moringaschool.com>
2022-08-31 14:11:07 +0300

    assignment in progress

--- project.js
+++ project.js
@@ -1,4 +1,4 @@
-const options = {
+/*const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '23ef58c331msh68bf74e3a440d82p126104jsn83af19d76ef0',
@@ -9,11 +9,29 @@ const options = {
 fetch('https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=18&page=1&output_language=en&language=en', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
-	.catch(err => console.error(err));
+	.catch(err => console.error(err));*/
+    
 
-    const likeCount = getElementById('countLikes')
-    console.log(likeCount)
+   const likeCount = document.querySelector('#likeCount');
+   const dislikeCount = document.querySelector('#dislikeCount');
 
-    function likes(data){
-        
-    }
+   function count1(){
+    likeCount.value = parseInt(likeCount.value) + 1;
+   }
+
+   function count2(){
+    dislikeCount.value = parseInt(dislikeCount.value) + 1;
+   }
+   count1()
+   count2()
+
+  
+const text = document.getElementById('input');
+const btn = document.getElementById('submit');
+const result = document.getElementById('box');
+
+function output () {
+    result.innerHTML = text.value;
+}
+
+btn.addEventListener('click',output)
